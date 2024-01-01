ea0f30745499db8c2b7d444540c32616170da615
neilchetna <neelchetna18@gmail.com>
2024-01-02 02:03:11 +0530

    Added fastapi backend and implemented generate code endpoint

--- server/code_generation/service.py
+++ server/code_generation/service.py
@@ -1,3 +1,8 @@
 from server.code_generation.schemas import CodeCreate
-def create_code(code_request: CodeCreate):
-    ...
+from server.code_generation.utils import gpt_code_request
+
+def create_code(code_generate_options: CodeCreate):
+
+    generated_code = gpt_code_request(code_generate_options.ui_description)
+
+    return {"generated_code": generated_code,"language": "JSX"}
--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from server.code_generation.router import code_generation_router
 
 app = FastAPI(title="Dynamic UI", version="0.0.1")
@@ -10,6 +11,14 @@ async def root():
             "status": "Live",
             "version": "0.0.1"
             }
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(
